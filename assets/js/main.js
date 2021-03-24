c70939d1949d476f87ec2da7c79dcf6f40551c24
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-24 15:38:19 +0100

    inizio esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    imgs:[
+      'https://i1.wp.com/fotografiaartistica.it/wp-content/uploads/2011/09/hengkikoentjoro1-1.jpg?ssl=1',
+      'https://www.caitorino.it/media/media_gallery/img/foto_sergio_chiappino_9.jpg',
+      'https://lh3.googleusercontent.com/proxy/48NM-sTkcy4rBH2shcM9WUZJiX_5AbEgiQ8ajTHn5KtJuE5G1Om8qBTGYS7B2E8iDUWYAJMqwYjtKDIW3KDRKiwefA3PJxeO6jlywg'
+    ]
+  },
+  // methods:
+});
